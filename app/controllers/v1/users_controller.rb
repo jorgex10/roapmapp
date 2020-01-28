1bd2f3440386ea2ab0d77ce321b8a7f9a84392be
Jorge Calvo <jorgeecg10@gmail.com>
2020-01-28 18:58:21 -0500

    Adding edit endpoint for users

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -19,7 +19,17 @@ module V1
     def create
       @user = User.new(user_params)
       @user.company = current_company
-      if @user .save
+      if @user.save
+        render json: @user, serializer: UserSerializer
+      else
+        render json: ErrorResponse.unprocessable_entity(@user), status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @user = User.find_by(id: params[:id])
+      @user.assign_attributes(user_params)
+      if @user.save
         render json: @user, serializer: UserSerializer
       else
         render json: ErrorResponse.unprocessable_entity(@user), status: :unprocessable_entity
